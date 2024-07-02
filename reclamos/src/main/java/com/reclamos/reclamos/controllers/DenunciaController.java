a22f3156ee8f60a2f85a0fcb35139fad044249ad
KanyeIsTheGoat <marcoojulianoo@gmail.com>
2024-07-02 20:30:59 -0300

    muchas cosas

--- reclamos/src/main/java/com/reclamos/reclamos/controllers/DenunciaController.java
+++ reclamos/src/main/java/com/reclamos/reclamos/controllers/DenunciaController.java
@@ -1,13 +1,22 @@
 package com.reclamos.reclamos.controllers;
+
+import com.reclamos.reclamos.enums.EstadoDenuncia;
+import com.reclamos.reclamos.enums.TipoDenuncia;
 import com.reclamos.reclamos.model.Denuncia;
 import com.reclamos.reclamos.services.DenunciaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 import java.util.List;
 
 @RestController
 @RequestMapping("api/denuncias")
 public class DenunciaController {
+
     @Autowired
     private DenunciaService denunciaService;
 
@@ -35,5 +44,25 @@ public class DenunciaController {
     public void deleteDenuncia(@PathVariable Long id) {
         denunciaService.deleteDenuncia(id);
     }
-}
 
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(
+            @RequestParam("descripcion") String descripcion,
+            @RequestParam("estadoDenuncia") EstadoDenuncia estadoDenuncia,
+            @RequestParam("tipoDenuncia") TipoDenuncia tipoDenuncia,
+            @RequestParam("titulo") String titulo,
+            @RequestParam("causa") String causa,
+            @RequestParam("lugar") String lugar,
+            @RequestParam("sitioId") Long sitioId,
+            @RequestParam("denunciadoId") Long denunciadoId,
+            @RequestParam("denuncianteId") Long denuncianteId,
+            @RequestParam("inspectorId") Long inspectorId,
+            @RequestParam("file") MultipartFile file) {
+        try {
+            denunciaService.saveDenuncia(descripcion, estadoDenuncia, tipoDenuncia, titulo, causa, lugar, sitioId, denunciadoId, denuncianteId, inspectorId, file);
+            return ResponseEntity.status(HttpStatus.OK).body("File uploaded successfully");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload file");
+        }
+    }
+}
--- reclamos/src/main/java/com/reclamos/reclamos/services/DenunciaService.java
+++ reclamos/src/main/java/com/reclamos/reclamos/services/DenunciaService.java
@@ -1,8 +1,20 @@
 package com.reclamos.reclamos.services;
+
 import com.reclamos.reclamos.model.Denuncia;
+import com.reclamos.reclamos.enums.EstadoDenuncia;
+import com.reclamos.reclamos.model.Inspector;
+import com.reclamos.reclamos.model.Sitio;
+import com.reclamos.reclamos.enums.TipoDenuncia;
+import com.reclamos.reclamos.model.Vecino;
 import com.reclamos.reclamos.repositorios.DenunciaRepository;
+import com.reclamos.reclamos.repositorios.InspectorRepository;
+import com.reclamos.reclamos.repositorios.SitioRepository;
+import com.reclamos.reclamos.repositorios.VecinoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -10,6 +22,15 @@ public class DenunciaService {
 
     @Autowired
     private DenunciaRepository denunciaRepository;
+
+    @Autowired
+    private SitioRepository sitioRepository;
+
+    @Autowired
+    private VecinoRepository vecinoRepository;
+
+    @Autowired
+    private InspectorRepository inspectorRepository;
 
     public List<Denuncia> getAllDenuncias() {
         return denunciaRepository.findAll();
@@ -33,5 +54,42 @@ public class DenunciaService {
 
     public void deleteDenuncia(Long id) {
         denunciaRepository.deleteById(id);
+    }
+
+    public void saveDenuncia(String descripcion, EstadoDenuncia estadoDenuncia, TipoDenuncia tipoDenuncia, String titulo, String causa, String lugar, Long sitioId, Long denunciadoId, Long denuncianteId, Long inspectorId, MultipartFile file) throws IOException {
+        Denuncia denuncia = new Denuncia();
+        denuncia.setDescripcion(descripcion);
+        denuncia.setEstadoDenuncia(estadoDenuncia);
+        denuncia.setTipoDenuncia(tipoDenuncia);
+        denuncia.setTitulo(titulo);
+        denuncia.setCausa(causa);
+        denuncia.setLugar(lugar);
+
+        if (sitioId != null) {
+            Sitio sitio = sitioRepository.findById(sitioId).orElse(null);
+            denuncia.setSitioDenunciado(sitio);
+        }
+
+        if (denunciadoId != null) {
+            Vecino denunciado = vecinoRepository.findById(denunciadoId).orElse(null);
+            denuncia.setDenunciado(denunciado);
+        }
+
+        if (denuncianteId != null) {
+            Vecino denunciante = vecinoRepository.findById(denuncianteId).orElse(null);
+            denuncia.setDenunciante(denunciante);
+        }
+
+        if (inspectorId != null) {
+            Inspector inspector = inspectorRepository.findById(inspectorId).orElse(null);
+            denuncia.setInspector(inspector);
+        }
+
+        if (file != null && !file.isEmpty()) {
+            byte[] pruebas = file.getBytes();
+            denuncia.setPruebas(pruebas);
+        }
+
+        denunciaRepository.save(denuncia);
     }
 }
